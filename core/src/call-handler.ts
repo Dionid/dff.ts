3f65e4bbe2476884db750ab54ef988db673e3d1e
David Shek <ditreyw@gmail.com>
2023-03-15 20:04:18 +0000

    feat: added new core

--- core/src/call-handler.ts
+++ core/src/call-handler.ts
@@ -1,52 +1,56 @@
-import { ReactiveCounter } from '@fddf-ts/core/reactive-counter'
+import { Call, CallsRecord, RequestFromCall, ResponseFromCall } from './call'
+import { EventsRecord } from './event'
+import { CallsToPublish, EventsToPublish } from './publishers'
 
-import { Call, DependantCalls } from './call'
+export type CallHandlerRun<
+  C extends Call<any, any, any>, // 100% because need to define Call
+  Ctx extends Record<any, any>, // 100% because need to define Ctx from outside
+  ER extends EventsRecord | undefined,
+  CR extends CallsRecord | undefined
+> = (
+  request: RequestFromCall<C>,
+  ctx: Ctx,
+  transport: {
+    call: CR extends CallsRecord ? CallsToPublish<CR> : unknown
+    event: ER extends EventsRecord ? EventsToPublish<ER> : unknown
+  }
+) => Promise<ResponseFromCall<C>>
 
 export type CallHandler<
+  C extends Call<any, any, any>,
   Ctx extends Record<any, any>,
-  Deps extends Record<string, Call<any, any, any, any>>,
-  Cl extends Call<any, any, any, any>
+  ER extends EventsRecord | undefined,
+  CR extends CallsRecord | undefined
 > = {
-  call: Cl
-  depCalls: Deps
-  persistent: boolean
-  handler: (
-    req: ReturnType<Cl['request']>,
-    ctx: Ctx,
-    depCalls: DependantCalls<Deps>
-  ) => Promise<ReturnType<Cl['result']>>
+  run: CallHandlerRun<C, Ctx, ER, CR>
+  isCallHandler: true
+  call: C
+  handlerName: string
+  callsToPublish?: CR
+  eventsToPublish?: ER
+  parallel?: number
 }
 
 export const CallHandler = <
+  C extends Call<string, any, any>,
   Ctx extends Record<any, any>,
-  Deps extends Record<string, Call<any, any, any, any>>,
-  Cl extends Call<any, any, any, any>
+  ER extends EventsRecord | undefined = undefined,
+  CR extends CallsRecord | undefined = undefined
 >(props: {
-  call: Cl
-  depCalls: Deps
-  persistent?: boolean
-  reactiveCounter?: ReactiveCounter
-  handler: (
-    req: ReturnType<Cl['request']>,
-    ctx: Ctx,
-    depCalls: DependantCalls<Deps>
-  ) => Promise<ReturnType<Cl['result']>>
-}): CallHandler<Ctx, Deps, Cl> => {
-  const { reactiveCounter } = props
-
+  call: C
+  callsToPublish?: CR
+  eventsToPublish?: ER
+  handlerName?: string
+  parallel?: number
+  run: CallHandlerRun<C, Ctx, ER, CR>
+}): CallHandler<C, Ctx, ER, CR> => {
   return {
-    ...props,
-    persistent: props.persistent === undefined ? false : props.persistent,
-    handler: reactiveCounter
-      ? async (req, ctx, depCalls) => {
-          reactiveCounter.increment()
-
-          try {
-            return props.handler(req, ctx, depCalls)
-          } finally {
-            reactiveCounter.decrement()
-          }
-        }
-      : props.handler
+    run: props.run,
+    call: props.call,
+    isCallHandler: true as const,
+    callsToPublish: props.callsToPublish,
+    eventsToPublish: props.eventsToPublish,
+    parallel: props.parallel,
+    handlerName: props.handlerName ?? `${props.call.name}CallHandler`
   }
 }
--- core/src/call.ts
+++ core/src/call.ts
@@ -1,132 +1,50 @@
-import { JSONObject, JSONValue } from '@fddf-ts/core/jsonvalue'
 import { v4 } from 'uuid'
 
-export type CallErrorResponseErrorBase<
-  Code extends number = number,
-  Message extends string = string,
-  Data extends Record<any, any> | undefined = undefined
-> = {
-  code: Code
-  message: Message
-  data?: Data
-}
+export type Empty = Record<string, never>
 
-export type CallServerErrorResponseError = CallErrorResponseErrorBase<500, 'Internal Server Error'>
-export type CallRequestTimeoutErrorResponseError = CallErrorResponseErrorBase<408, 'Request timeout'>
-export type CallNotFoundErrorResponseError = CallErrorResponseErrorBase<404>
-export type CallForbiddenErrorResponseError = CallErrorResponseErrorBase<403>
-export type CallBadRequestErrorResponseError = CallErrorResponseErrorBase<422>
-
-export type CallErrorResponseError =
-  | CallServerErrorResponseError
-  | CallNotFoundErrorResponseError
-  | CallForbiddenErrorResponseError
-  | CallBadRequestErrorResponseError
-  | CallRequestTimeoutErrorResponseError
-
-export type CallErrorResponse<RespError extends CallErrorResponseErrorBase = CallErrorResponseError> = {
-  id: string
-  error: RespError
-}
-
-export type CallRequest<Name extends string = string, ReqParams extends JSONObject = JSONObject> = {
+export type CallRequest<Name extends string, Params extends Record<any, any>> = {
   id: string
   name: Name
-  params: ReqParams
+  params: Params
 }
 
-export type CallResponseResult<RespResult extends JSONObject = JSONObject> = {
+export type CallResponse<Result extends Record<any, any>> = {
   id: string
-  result: RespResult
+  result: Result
 }
 
-export type CallResponse<
-  RespResult extends JSONObject = JSONObject,
-  RespError extends CallErrorResponseErrorBase = CallErrorResponseError
-> = CallResponseResult<RespResult> | CallErrorResponse<RespError>
-
-export type Call<
-  Name extends string = string,
-  ReqParams extends JSONObject = JSONObject,
-  RespResult extends JSONObject = JSONObject,
-  RespError extends CallErrorResponseErrorBase = CallErrorResponseError
-> = {
+export type Call<Name extends string, Request extends CallRequest<Name, any>, Response extends CallResponse<any>> = {
   name: Name
-  requestParser?: RequestParser<ReqParams>
-  sideEffects?: boolean
-  request: (params: ReqParams, id?: string) => CallRequest<Name, ReqParams>
-  result: (
-    props: { id: string; result: RespResult } | { req: CallRequest<Name, ReqParams>; result: RespResult }
-  ) => CallResponseResult<RespResult>
-  error: (
-    props: { id: string; error: RespError } | { req: CallRequest<Name, ReqParams>; error: RespError }
-  ) => CallErrorResponse<RespError>
+  request: (...args: any[]) => Request
+  response: (...args: any[]) => Response
 }
 
-type RPF = (propName: string, val: JSONValue) => Promise<JSONValue> | JSONValue
+// # CONSTRUCTOR
 
-export type RequestParser<ReqParams extends JSONObject> = {
-  [K in keyof ReqParams]: ReqParams[K] extends JSONObject ? RequestParser<ReqParams[K]> : RPF
-}
-
-export const Call = <
-  Name extends string = string,
-  ReqParams extends JSONObject = JSONObject,
-  RespResult extends JSONObject = JSONObject,
-  RespError extends CallErrorResponseErrorBase = CallErrorResponseError
->(
-  name: Name,
-  props: {
-    sideEffects?: boolean
-    requestParser?: RequestParser<ReqParams> // TODO. Create from ReqParams
-  } = {}
-): Call<Name, ReqParams, RespResult, RespError> => {
-  const { sideEffects = false, requestParser } = props
-
+export const Call = <Name extends string, Request extends CallRequest<Name, any>, Response extends CallResponse<any>>(
+  name: Name
+) => {
   return {
     name,
-    requestParser,
-    sideEffects,
-    request: (params: ReqParams, id?: string) => {
+    request: (request: Omit<Request, 'id' | 'name'>) => {
       return {
-        id: id || v4(),
+        id: v4(),
         name,
-        params
-      }
+        ...request
+      } as Request
     },
-    result: (props: { id: string; result: RespResult } | { req: CallRequest<Name, ReqParams>; result: RespResult }) => {
-      const { result } = props
-
-      if ('req' in props) {
-        return {
-          id: props.req.id,
-          result
-        }
-      } else {
-        return {
-          id: props.id,
-          result
-        }
-      }
-    },
-    error: (props: { id: string; error: RespError } | { req: CallRequest<Name, ReqParams>; error: RespError }) => {
-      const { error } = props
-
-      if ('req' in props) {
-        return {
-          id: props.req.id,
-          error
-        }
-      } else {
-        return {
-          id: props.id,
-          error
-        }
-      }
+    response: (idOrRequest: string | Request, response: Omit<Response, 'id'>) => {
+      return {
+        id: typeof idOrRequest === 'string' ? idOrRequest : idOrRequest.id,
+        ...response
+      } as Response
     }
   }
 }
 
-export type DependantCalls<Deps extends Record<string, Call<any, any, any, any>>> = {
-  [K in keyof Deps]: (request: ReturnType<Deps[K]['request']>) => Promise<ReturnType<Deps[K]['result']>>
-}
+// # UTILS
+
+export type RequestFromCall<C extends Call<any, any, any>> = ReturnType<C['request']>
+export type ResponseFromCall<C extends Call<any, any, any>> = ReturnType<C['response']>
+
+export type CallsRecord = Record<string, Call<string, any, any>>
